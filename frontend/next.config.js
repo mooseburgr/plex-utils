ebe9c1349f7ef5ea0adb3cb32931d69404ec4a99
Kyle Johnson <kylejohnson.mn@gmail.com>
2022-07-05 16:13:59 -0500

    Add test edge function, rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
   assetPrefix: './',
-}
+  async rewrites() {
+    return [
+      {
+        source: '/cr',
+        destination: 'https://cloudrun-srv-d3zjrv6fua-uc.a.run.app',
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/pw',
+        destination: 'https://www.polywork.com/mooseburger',
+        permanent: false,
+      },
+    ];
+  }
+};
 
 module.exports = nextConfig
