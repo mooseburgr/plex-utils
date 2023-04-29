894a24aaa7bc8160929b68f739d07619872bb841
Kyle Johnson <kylejohnson.mn@gmail.com>
2023-04-29 15:11:03 -0500

    add Vercel Analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Analytics} from '@vercel/analytics/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   return (
-    <Component {...pageProps} />
+      <>
+        <Component {...pageProps} />
+        <Analytics/>
+      </>
   )
 }
 
